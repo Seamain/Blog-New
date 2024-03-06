8dc9f35e860c744f213ba6218794f730db31c867
Seamain <quanminjun37@gmail.com>
2024-03-06 18:25:04 +0800

    Add Vercel serverless adapter and enable web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [tailwind()],
   site: 'https://seamain.org'
 });

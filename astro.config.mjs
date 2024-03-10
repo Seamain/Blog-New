d25ab86406acc45370aacbf5bbcbaaba28157b02
Seamain <quanminjun37@gmail.com>
2024-03-10 15:28:18 +0800

    Add sitemap integration and robots.txt route

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://seamain.org'
 });

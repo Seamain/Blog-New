38242054e0bcc51dfc12517c603d1723095e74dc
Seamain <quanminjun37@gmail.com>
2024-04-11 15:22:15 +0800

    Update tsconfig.json to include "jsx": "react"

---
+++ src/templates/og-templates/post.tsx
@@ -0,0 +1,62 @@
+import type { CollectionEntry } from "astro:content";
+
+export default (post: CollectionEntry<"blog">) => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                height: '100%',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                letterSpacing: '-.02em',
+                fontWeight: 700,
+                background: 'white',
+            }}
+        >
+            <div
+                style={{
+                    left: 42,
+                    top: 42,
+                    position: 'absolute',
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+            >
+                <span
+                    style={{
+                        width: 24,
+                        height: 24,
+                        background: 'black',
+                    }}
+                />
+                <span
+                    style={{
+                        marginLeft: 8,
+                        fontSize: 20,
+                    }}
+                >
+                    seamain.org
+                </span>
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center',
+                    padding: '20px 50px',
+                    margin: '0 42px',
+                    fontSize: 40,
+                    width: 'auto',
+                    maxWidth: 550,
+                    textAlign: 'center',
+                    backgroundColor: 'black',
+                    color: 'white',
+                    lineHeight: 1.4,
+                }}
+            >
+                {post.data.title}
+            </div>
+        </div>
+    );
+};

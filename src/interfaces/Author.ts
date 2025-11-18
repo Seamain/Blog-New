c818769c52c89c7513b2e06c9faae9a4d391257f
Seamain <quanminjun37@gmail.com>
2025-11-18 09:32:57 +0800

    反向同步Develop (#19)
    * Bump follow-redirects from 1.15.3 to 1.15.6 (#2)
    
    Bumps [follow-redirects](https://github.com/follow-redirects/follow-redirects) from 1.15.3 to 1.15.6.
    - [Release notes](https://github.com/follow-redirects/follow-redirects/releases)
    - [Commits](https://github.com/follow-redirects/follow-redirects/compare/v1.15.3...v1.15.6)
    
    ---
    updated-dependencies:
    - dependency-name: follow-redirects
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Seamain <quanminjun37@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Seamain <quanminjun37@gmail.com>
    
    * Bump nanoid from 3.3.7 to 3.3.8 (#4)
    
    Bumps [nanoid](https://github.com/ai/nanoid) from 3.3.7 to 3.3.8.
    - [Release notes](https://github.com/ai/nanoid/releases)
    - [Changelog](https://github.com/ai/nanoid/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/ai/nanoid/compare/3.3.7...3.3.8)
    
    ---
    updated-dependencies:
    - dependency-name: nanoid
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump vite from 6.0.6 to 6.2.5 (#7)
    
    Bumps [vite](https://github.com/vitejs/vite/tree/HEAD/packages/vite) from 6.0.6 to 6.2.5.
    - [Release notes](https://github.com/vitejs/vite/releases)
    - [Changelog](https://github.com/vitejs/vite/blob/v6.2.5/packages/vite/CHANGELOG.md)
    - [Commits](https://github.com/vitejs/vite/commits/v6.2.5/packages/vite)
    
    ---
    updated-dependencies:
    - dependency-name: vite
      dependency-version: 6.2.5
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore: update dependencies and refactor API interfaces
    
    - Updated dependencies in package.json for various packages including @astrojs/rss, @astrojs/sitemap, and others.
    - Refactored API calls in About.ts, Category.ts, Friend.ts, and Post.ts to remove the use of Attribute interface, simplifying data handling.
    - Adjusted PostCard.astro to handle avatar URLs more gracefully.
    - Removed the Attribute interface as it was no longer needed.
    - Updated the Author, Category, and Post interfaces to reflect changes in data structure.
    - Modified category filtering logic in [...page].astro to align with new data structure.
    - Updated feed.xml.js to reflect changes in post data structure.
    - Adjusted friends.astro to access friend properties directly.
    - Simplified index.astro and [slug].astro to work with the new data structure.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Seamain <quanminjun37@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/interfaces/Author.ts
+++ src/interfaces/Author.ts
@@ -1,23 +1,21 @@
 import type { Optional } from "./../types/Optional";
-import type Attribute from "./Attribute";
-import type Data from "./Data";
 import type Media from "./media/Media";
 
 export default interface Author {
   slug: string;
   name: string;
-  avatar: Optional<Data<Attribute<Media>>>;
+  avatar: Optional<Media>;
   site: string;
   contacts: {
     type:
-      | "Email"
-      | "Phone"
-      | "Weixin"
-      | "WeChat"
-      | "QQ"
-      | "Skype"
-      | "Telegram"
-      | "Line";
+    | "Email"
+    | "Phone"
+    | "Weixin"
+    | "WeChat"
+    | "QQ"
+    | "Skype"
+    | "Telegram"
+    | "Line";
     value: string;
   }[];
   publicKey: string;

bbfbbd86ccb55ea8d07dae69580f88b9a5814f4b
Seamain <quanminjun37@gmail.com>
2025-11-19 15:33:55 +0800

    chore: update dependencies and add global styles
    - Removed @astrojs/tailwind and added @tailwindcss/vite for Tailwind CSS integration.
    - Updated tailwindcss to version 4.1.17.
    - Imported global CSS file in BasicStyle.astro to include Tailwind CSS styles.
    - Created global.css file to import Tailwind CSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [react(), sitemap()],
   site: 'https://seamain.org',
   vite: {
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
     },
+
+    plugins: [tailwindcss()],
   },
 });
